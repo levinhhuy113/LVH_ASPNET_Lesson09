7aa6f00e09e90ed9787d4f6c5e024e41b65d5467
unknown <levinhhuy113@gmail.com>
2024-06-17 10:22:14 +0700

    add file

---
+++ LvhLesson09/LvhLesson09/Controllers/LvhKhoasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LvhLesson09.Models;
+
+namespace LvhLesson09.Controllers
+{
+    public class LvhKhoasController : Controller
+    {
+        private LvhK22CNT1Lesson07DbEntities2 db = new LvhK22CNT1Lesson07DbEntities2();
+
+        // GET: LvhKhoas
+        public ActionResult LvhIndex()
+        {
+            return View(db.lvhKhoas.ToList());
+        }
+
+        // GET: LvhKhoas/Details/5
+        public ActionResult LvhDetails(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            lvhKhoa lvhKhoa = db.lvhKhoas.Find(id);
+            if (lvhKhoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lvhKhoa);
+        }
+
+        // GET: LvhKhoas/Create
+        public ActionResult LvhCreate()
+        {
+            return View();
+        }
+
+        // POST: LvhKhoas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhCreate([Bind(Include = "LvhMaKH,LvhTenKH,LvhTrangThai")] lvhKhoa lvhKhoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.lvhKhoas.Add(lvhKhoa);
+                db.SaveChanges();
+                return RedirectToAction("LvhIndex");
+            }
+
+            return View(lvhKhoa);
+        }
+
+        // GET: LvhKhoas/Edit/5
+        public ActionResult LvhEdit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            lvhKhoa lvhKhoa = db.lvhKhoas.Find(id);
+            if (lvhKhoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lvhKhoa);
+        }
+
+        // POST: LvhKhoas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhEdit([Bind(Include = "LvhMaKH,LvhTenKH,LvhTrangThai")] lvhKhoa lvhKhoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lvhKhoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("LvhIndex");
+            }
+            return View(lvhKhoa);
+        }
+
+        // GET: LvhKhoas/Delete/5
+        public ActionResult LvhDelete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            lvhKhoa lvhKhoa = db.lvhKhoas.Find(id);
+            if (lvhKhoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lvhKhoa);
+        }
+
+        // POST: LvhKhoas/Delete/5
+        [HttpPost, ActionName("LvhDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhDeleteConfirmed(string id)
+        {
+            lvhKhoa lvhKhoa = db.lvhKhoas.Find(id);
+            db.lvhKhoas.Remove(lvhKhoa);
+            db.SaveChanges();
+            return RedirectToAction("LvhIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LvhLesson09/LvhLesson09/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LvhLesson09.MvcApplication" Language="C#" %>
